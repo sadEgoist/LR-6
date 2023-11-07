dfee4151b8af93aabecb975df8229cb4fd44b63c
dsafg <126382125+sadEgoist@users.noreply.github.com>
2023-11-07 11:22:38 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        ThreadTest test = new ThreadTest();
+        test.methodOne();
+        test.methodTwo();
+        test.methodThree(4);
+    }
+}
